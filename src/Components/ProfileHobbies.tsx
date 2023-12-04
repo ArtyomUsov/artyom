5f496b5b271100cad32e60e345e7427244536474
Usov_Artem <artemka063@gmail.com>
2023-12-04 18:24:02 +0400

    feat: add hobbies

--- src/Components/ProfileHobbies.tsx
+++ src/Components/ProfileHobbies.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function ProfileHobbies() {
-
-    return (
-        <>
-
-        </>
-    )
+  return (
+    <>
+      <h3>Хобби:</h3>
+      <span>Настольные игры, </span>
+      <span>Программирование, </span>
+      <span>Макетирование. </span>
+    </>
+  );
 }
