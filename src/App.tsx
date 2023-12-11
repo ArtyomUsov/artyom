4d27d1555145f3d8775dc418fd68369a10b0c646
Usov_Artem <artemka063@gmail.com>
2023-12-11 17:50:40 +0400

    init: start Radio Applications

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styles from "./App.module.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <h1>Заявки на Радио</h1>
+      <h3>Отправляйте заявку и мы сыграем вашу любимую песню!</h3>
+      <form>
+        <input type="text" placeholder="Название песни" />
+        <input type="text" placeholder="исполнитель" />
+        <select>
+          <option value="rock">Рок</option>
+          <option value="pop">Поп</option>
+          <option value="hip-hop">Блюз</option>
+        </select>
+        <button type="submit">Отправить</button>
+        <div className={styles.songList}>
+          <p>Песня 1 - Исполнитель 1 - Жанр 1</p>
+          <hr />
+          <p>Песня 2 - Исполнитель 2 - Жанр 2</p>
+          <hr />
+          <p>Песня 1 - Исполнитель 1 - Жанр 1</p>
+          <hr />
+          <p>Песня 2 - Исполнитель 2 - Жанр 2</p>
+          <hr />
+          <p>Песня 1 - Исполнитель 1 - Жанр 1</p>
+          <hr />
+          <p>Песня 2 - Исполнитель 2 - Жанр 2</p>
+          <hr />
+        </div>
+      </form>
     </div>
   );
 }
-
-export default App;
